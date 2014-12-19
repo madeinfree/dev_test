86b3355d5674e03bd479f4f40772f4ea056a83a8
jzd-tsai <jzd.tsai@gmail.com>
2014-12-19 23:22:44 +0800

    用較正確的方式把cart<->product.id關聯存進items了

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,17 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
-    @carts = CartItem.where(:cart_id => session[:cart])
+    @carts = CartItem.where(cart_id: current_cart.id)
   end
   def show
     @product = Product.find(params[:id])
   end
   def add_to_cart
-    current_cart
     #show Cart
-    #redirect_to products_url
+    @product = Product.find(params[:id])
+    @add_item = CartItem.create(cart_id: current_cart.id, product_id: @product.id, prince: @product.price)
+    #current_cart.add_product_to_cart(@product)
+    
+    redirect_to products_url
   end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,13 +1,2 @@
 module ProductsHelper
-  # helper_method :current_cart
-  def current_cart
-    @current_cart ||= find_cart
-  end
-  def find_cart
-    cart = Cart.find_by(:id => session[:cart_id])
-    unless cart.present?
-      cart = Cart.create
-    end
-    session[:cart_id] = cart.id
-  end
 end
